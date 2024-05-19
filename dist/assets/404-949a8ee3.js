228d42a0f9d9d150c43a0e0b21e53abf0297623d
yc <yangchen@qq.com>
2024-05-19 17:14:43 +0800

    docs

---
+++ dist/assets/404-949a8ee3.js
@@ -0,0 +1,1 @@
+import{_ as e,o as t,c as o}from"./index-259b60a9.js";const r={created(){this.$router.push({path:"/"})}};function c(s,a,n,p,_,d){return t(),o("div")}const i=e(r,[["render",c]]);export{i as default};
